2228d4e18665890daf4f7edf51d5ec286d28744f
Demian <devaccdemiann@gmail.com>
2019-10-26 21:53:04 +0200

    Created anime gif  todo api key

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,8 @@
 module.exports = {
-    owner: '192758351812231168',
-    bot_token: 'NTk1Mjc3NDQ4MDk5MjY2NTkx.XRopYw.Q2WV7300ye6MjsVTjGGbOGmdAYE',
-    bot_id: '595277448099266591',
-    bot_invite_link: 'https://discordapp.com/oauth2/authorize?client_id=595277448099266591&scope=bot&permissions=8'
-}
+	owner: "192758351812231168",
+	bot_token: "NTk1Mjc3NDQ4MDk5MjY2NTkx.XRopYw.Q2WV7300ye6MjsVTjGGbOGmdAYE",
+	bot_id: "595277448099266591",
+	bot_invite_link:
+		"https://discordapp.com/oauth2/authorize?client_id=595277448099266591&scope=bot&permissions=8",
+	youtubeAPI: "AIzaSyAvJrakLAyr8tH0QtBQJWjQ8fwu9SoZ8MA",
+};
