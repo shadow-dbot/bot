56e9d522210761737df81e9f2935cb7f1fb02017
Demian <devaccdemiann@gmail.com>
2019-10-26 15:09:27 +0200

    Added configs

---
+++ config/key.js
@@ -0,0 +1,9 @@
+if(!process.env.NODE_ENV) {
+    return module.exports = require("./dev.js")
+} 
+module.exports = {
+    owner: '192758351812231168',
+    bot_token: process.env.BOT_TOKEN,
+    bot_id: process.env.BOT_ID,
+    bot_invite_link: process.env.BOT_INVITE_LINK
+}
