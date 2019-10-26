ff69240d5f68c390ce8d0dd22a019acb7caa7180
Demian <devaccdemiann@gmail.com>
2019-10-26 22:56:15 +0200

    Added api keys

--- config/key.js
+++ config/key.js
@@ -1,9 +1,12 @@
-if(!process.env.NODE_ENV) {
-    return module.exports = require("./dev.js")
-} 
-module.exports = {
-    owner: '192758351812231168',
-    bot_token: process.env.BOT_TOKEN,
-    bot_id: process.env.BOT_ID,
-    bot_invite_link: process.env.BOT_INVITE_LINK
+if (!process.env.NODE_ENV) {
+	return (module.exports = require("./dev.js"));
 }
+module.exports = {
+	owner: "192758351812231168",
+	bot_token: process.env.BOT_TOKEN,
+	bot_id: process.env.BOT_ID,
+	bot_invite_link: process.env.BOT_INVITE_LINK,
+	youtubeAPI: process.env.YOUTUBE_API,
+	tenorAPI: process.env.TENOR_API,
+	newsAPI: process.env.news_API,
+};
