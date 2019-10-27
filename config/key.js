c493e805a0070ceaf5d57f18105332ce1e90810d
Demian <devaccdemiann@gmail.com>
2019-10-27 12:29:33 +0100

    Updated

--- config/key.js
+++ config/key.js
@@ -1,6 +1,6 @@
 // if (!process.env.NODE_ENV) {
 // return (
-module.exports = require("./dev.js");
+// module.exports = require("./dev.js");
 // );
 // }
 // module.exports = {
@@ -12,3 +12,14 @@ module.exports = require("./dev.js");
 // 	tenorAPI: process.env.TENOR_API,
 // 	newsAPI: process.env.NEWS_API,
 // };
+
+module.exports = {
+	owner: "192758351812231168",
+	bot_token: "NTk1Mjc3NDQ4MDk5MjY2NTkx.XRopYw.Q2WV7300ye6MjsVTjGGbOGmdAYE",
+	bot_id: "595277448099266591",
+	bot_invite_link:
+		"https://discordapp.com/oauth2/authorize?client_id=595277448099266591&scope=bot&permissions=8",
+	youtubeAPI: "AIzaSyAvJrakLAyr8tH0QtBQJWjQ8fwu9SoZ8MA",
+	tenorAPI: "KQWG0Q5XDY5K",
+	newsAPI: "6e5afdea909e4d048ebffaf77080bd42",
+};
