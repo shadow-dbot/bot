11e2f16bf2d35f774f22cb20588fc39490b11049
Demian <devaccdemiann@gmail.com>
2019-10-27 13:15:57 +0100

    Added 2 different prefixes for dev and prod

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
 	owner: "192758351812231168",
+	prefix: "*",
 	bot_token: "NTk1Mjc3NDQ4MDk5MjY2NTkx.XRopYw.Q2WV7300ye6MjsVTjGGbOGmdAYE",
 	bot_id: "595277448099266591",
 	bot_invite_link:
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
 	owner: "192758351812231168",
+	prefix: "$",
 	bot_token: process.env.BOT_TOKEN,
 	bot_id: process.env.BOT_ID,
 	bot_invite_link: process.env.BOT_INVITE_LINK,
