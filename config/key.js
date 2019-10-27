c915720be0e584e3e59ea177b969b798f912eb3c
Demian <devaccdemiann@gmail.com>
2019-10-27 12:49:22 +0100

    Updated

--- config/key.js
+++ config/key.js
@@ -1,25 +1,12 @@
-// if (!process.env.NODE_ENV) {
-// return (
-// module.exports = require("./dev.js");
-// );
-// }
-// module.exports = {
-// 	owner: "192758351812231168",
-// 	bot_token: process.env.BOT_TOKEN,
-// 	bot_id: process.env.BOT_ID,
-// 	bot_invite_link: process.env.BOT_INVITE_LINK,
-// 	youtubeAPI: process.env.YOUTUBE_API,
-// 	tenorAPI: process.env.TENOR_API,
-// 	newsAPI: process.env.NEWS_API,
-// };
-
+if (!process.env.NODE_ENV) {
+	return (module.exports = require("./dev.js"));
+}
 module.exports = {
 	owner: "192758351812231168",
-	bot_token: "NTk1Mjc3NDQ4MDk5MjY2NTkx.XRopYw.Q2WV7300ye6MjsVTjGGbOGmdAYE",
-	bot_id: "595277448099266591",
-	bot_invite_link:
-		"https://discordapp.com/oauth2/authorize?client_id=595277448099266591&scope=bot&permissions=8",
-	youtubeAPI: "AIzaSyAvJrakLAyr8tH0QtBQJWjQ8fwu9SoZ8MA",
-	tenorAPI: "KQWG0Q5XDY5K",
-	newsAPI: "6e5afdea909e4d048ebffaf77080bd42",
+	bot_token: process.env.BOT_TOKEN,
+	bot_id: process.env.BOT_ID,
+	bot_invite_link: process.env.BOT_INVITE_LINK,
+	youtubeAPI: process.env.YOUTUBE_API,
+	tenorAPI: process.env.TENOR_API,
+	newsAPI: process.env.NEWS_API,
 };
