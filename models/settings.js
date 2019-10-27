ee0d404df07859e336590dcaff40f6ec12131f0d
Demian <devaccdemiann@gmail.com>
2019-10-27 21:25:46 +0100

    Added wellcome and profanity

--- models/settings.js
+++ models/settings.js
@@ -5,6 +5,26 @@ const settingsModel = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	welcome: {
+		msg: {
+			type: Boolean,
+			default: false,
+		},
+		channel: {
+			type: String,
+			default: "general",
+		},
+	},
+	profanity: {
+		filter: {
+			type: Boolean,
+			default: false,
+		},
+		words: {
+			type: Array,
+			default: ["fuck"],
+		},
+	},
 });
 
 const Settings = mongoose.model("Settings", settingsModel);
