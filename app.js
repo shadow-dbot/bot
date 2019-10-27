034cda59580920a86eb00cda9dd3d5279f43a758
Demian <devaccdemiann@gmail.com>
2019-10-27 20:44:39 +0100

    Added mongoose and models

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const { Structures } = require("discord.js");
 const Commando = require("discord.js-commando");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const Command = require("./models/command");
+const Guilds = require("./models/server");
+const Settings = require("./models/settings");
 
 const Config = require("./config/key.js");
 
@@ -24,7 +29,44 @@ const client = new Commando.Client({
 	owner: Config.owner,
 });
 
+mongoose
+	.connect("mongodb://localhost:27017/discord-bot", {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then(_ => console.log("connected"))
+	.catch(e => console.error("error connecting mongodb"));
+
 client.on("ready", () => console.log("Ready"));
+
+client.on("guildCreate", async guild => {
+	let guilds = await Guilds.findOne({ guildID: guild.id });
+	let settings = await Settings.findOne({ guiildID: guild.id });
+	if (!guilds) console.log(guilds);
+
+	if (!settings) {
+		settings = new Settings({ guildID: guild.id });
+		settings.save();
+	}
+
+	const newServer = new Guilds({
+		guildID: guild.id,
+		ownerID: guild.ownerID,
+		region: guild.region,
+		name: guild.name,
+		icon: guild.icon,
+		settings: settings._id,
+	});
+	await newServer.save();
+});
+
+client.on("commandRun", async cmd => {
+	const newCommand = new Command({
+		type: cmd.name,
+		group_type: cmd.groupID,
+	});
+	await newCommand.save();
+});
 
 client.registry
 	.registerGroups([
