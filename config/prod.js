05d146cf2dcb4a90c878e44182bc3a57b234d5fb
Demian <devaccdemiann@gmail.com>
2019-10-27 13:01:32 +0100

    Changed keys

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+	owner: "192758351812231168",
+	bot_token: process.env.BOT_TOKEN,
+	bot_id: process.env.BOT_ID,
+	bot_invite_link: process.env.BOT_INVITE_LINK,
+	youtubeAPI: process.env.YOUTUBE_API,
+	tenorAPI: process.env.TENOR_API,
+	newsAPI: process.env.NEWS_API,
+};
