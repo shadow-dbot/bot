8e993046ba5e0c8810cc4137dfecc8a7c3c303ac
Demian <devaccdemiann@gmail.com>
2019-10-27 20:44:59 +0100

    Added invite command  (returns my inv link)

---
+++ commands/misc/invite.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const { Command } = require("discord.js-commando");
+const { bot_invite_link } = require("../../config/key");
+
+module.exports = class JokeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "invite",
+			aliases: ["invite", "invme", "botinv"],
+			group: "misc",
+			memberName: "invite",
+			description: "My invite link",
+			throttling: {
+				usages: 1,
+				duration: 60,
+			},
+		});
+	}
+
+	run(msg) {
+		msg.reply(bot_invite_link);
+	}
+};
