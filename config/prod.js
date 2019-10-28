4f3c4892634806f17a1bfea092ef0bfe8e69c6a9
Demian <devaccdemiann@gmail.com>
2019-10-28 20:23:31 +0100

    added bad practice

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,23 @@
+// module.exports = {
+// 	owner: "192758351812231168",
+// 	prefix: "$",
+// 	bot_token: process.env.BOT_TOKEN,
+// 	bot_id: process.env.BOT_ID,
+// 	bot_invite_link: process.env.BOT_INVITE_LINK,
+// 	youtubeAPI: process.env.YOUTUBE_API,
+// 	tenorAPI: process.env.TENOR_API,
+// 	newsAPI: process.env.NEWS_API,
+// };
+
 module.exports = {
 	owner: "192758351812231168",
 	prefix: "$",
-	bot_token: process.env.BOT_TOKEN,
-	bot_id: process.env.BOT_ID,
-	bot_invite_link: process.env.BOT_INVITE_LINK,
-	youtubeAPI: process.env.YOUTUBE_API,
-	tenorAPI: process.env.TENOR_API,
-	newsAPI: process.env.NEWS_API,
+	bot_id: "637985716768604160",
+	bot_invite_link:
+		"https://discordapp.com/oauth2/authorize?client_id=637985716768604160&scope=bot&permissions=8",
+	bot_token: "NjM3OTg1NzE2NzY4NjA0MTYw.XbWImQ.E_SRWBcjEhjAjiK-Hv6nSoSmQz0",
+	youtubeAPI: "AIzaSyAvJrakLAyr8tH0QtBQJWjQ8fwu9SoZ8MA",
+	tenorAPI: "KQWG0Q5XDY5K",
+	newsAPI: "6e5afdea909e4d048ebffaf77080bd42",
+	support_link: "https://discord.gg/cyRDWR8",
 };
