9ae308fca1bb90a492960bfbf3b58119f257a218
Demian <devaccdemiann@gmail.com>
2019-10-29 13:56:46 +0100

    Refactored

--- commands/music/skipto.js
+++ commands/music/skipto.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando");
 
-module.exports = class SkipToCommand extends Command {
+module.exports = class skipToCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "skipto",
