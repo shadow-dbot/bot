39feb2f745357822b8b057f9acb276ecc654d9f2
Demian <devaccdemiann@gmail.com>
2019-10-29 13:55:22 +0100

    Refactored

--- commands/misc/insult.js
+++ commands/misc/insult.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const { Command } = require("discord.js-commando");
 const { MessageEmbed } = require("discord.js");
 
-module.exports = class InsultCommand extends Command {
+module.exports = class insultCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "insult",
