385fcedb4997f9cb9b8e17eabea7ceac06ef3075
Demian <devaccdemiann@gmail.com>
2019-10-29 13:55:51 +0100

    Refactored

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando");
 
-module.exports = class PauseCommand extends Command {
+module.exports = class pauseCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "pause",
