cac80c0872e22f41d8d4a7a9d169574c421531d1
Demian <devaccdemiann@gmail.com>
2019-10-29 13:56:31 +0100

    Refactored

--- commands/music/shuffle.js
+++ commands/music/shuffle.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando");
 
-module.exports = class ShuffleQueueCommand extends Command {
+module.exports = class shuffleQueueCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "shuffle",
