37998d36b1ab1e15e5a0368e8e590b41b754fad3
Demian <devaccdemiann@gmail.com>
2019-10-29 13:56:02 +0100

    Refactored

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando");
 
-module.exports = class LeaveCommand extends Command {
+module.exports = class leaveCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "leave",
