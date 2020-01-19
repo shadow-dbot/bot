1e0c0b63a2f92be5b0da13c54d6a4ab24630b402
Demian <devaccdemiann@gmail.com>
2020-01-19 21:30:35 +0100

    Removed console.log on server post

--- bot/handlers/DBL.js
+++ bot/handlers/DBL.js
@@ -4,9 +4,7 @@ const postServerCount = client => {
 	if (process.env.NODE_ENV === "production") {
 		const dbl = new DBL(process.env.DBL_API_KEY, client);
 
-		dbl.on("posted", () => {
-			console.log("Server count posted!");
-		});
+		dbl.on("posted", () => {});
 
 		dbl.on("error", e => {
 			console.log(`Oops! ${e}`);
