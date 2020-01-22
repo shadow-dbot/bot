8a083071cfca27276e49457945302a03a19d4009
Demian <devaccdemiann@gmail.com>
2020-01-22 22:36:05 +0100

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const Handlers = require("./handlers");
-
 require("dotenv").config();
 
 require("./database");
+
+const Handlers = require("./handlers");
 
 const { Structures } = require("discord.js");
 
